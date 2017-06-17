65aad6dfd73b3baa219df6aca1064e23829b2be2
William Brown <firstyear@redhat.com>
2017-06-17 15:55:37 +1000

    Complete change how we build with autotools to remove crates.

--- complex_plugins/controller/src/main.rs
+++ complex_plugins/controller/src/main.rs
@@ -1,3 +1,29 @@
+extern crate dylib;
+
+use std::path::Path;
+use dylib::DynamicLibrary;
+
+
 fn main() {
     println!("Hello, world!");
+
+    // let libpath = Path::new("./libplugin_r.so");
+    let libpath = Path::new("../plugin_r/liblib.so");
+
+    match DynamicLibrary::open(Some(libpath)) {
+        Ok(handle) => {
+            println!("Opened!");
+            unsafe {
+                // Now we get dangerous: We have to unsafe access
+                // the raw pointer of the function.
+                let mfptr: *mut () = handle.symbol("plugin_init").unwrap();
+                let fptr = std::mem::transmute::<*mut (), fn() -> i32>(mfptr);
+                println!("Fn returns: {}", fptr());
+            }
+        },
+        Err(e) => println!("Failed to open: {}", e),
+    }
+
+
+
 }
--- complex_plugins/plugin_r/src/lib.rs
+++ complex_plugins/plugin_r/src/lib.rs
@@ -1,3 +1,13 @@
+
+#[no_mangle]
+pub extern fn plugin_init() -> i32 {
+    let xs = [0, 1, 2, 3];
+    println!("{:?}", xs);
+    let y = unsafe { *xs.as_ptr().offset(4) };
+    // 0
+    y
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
